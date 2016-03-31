745fb49f51e43b4156b8a03b83fdd807340c6a06
altacus <paultsai@gmail.com>
2016-03-31 19:36:11 -0400

    Checking in first assignment

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -11,5 +11,43 @@ namespace UnitTest
         {
             Assert.IsFalse(false);
         }
+
+        [TestMethod]
+        public void FirstTests()
+        {
+            int[] records = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int result = SumArray(records);
+            Assert.AreEqual(45, result);
+
+            /* Note this test fails, but I want you to make 
+               it pass by modifying the SumArray2 method*/
+            result = SumArray2(records);
+            Assert.AreEqual(45, result);
+        }
+
+        int SumArray(int[] arrayToSum)
+        {
+            int totalSum;
+            totalSum = 0;
+
+            foreach(int x in arrayToSum)
+            {
+                totalSum = totalSum + x;
+            }
+            return totalSum;
+        }
+
+        /// <summary>
+        /// Please implement the SumArray function, but instead of using a foreach loop,
+        /// use a for loop.
+        /// https://msdn.microsoft.com/en-us/library/ch45axte.aspx
+        /// http://www.dotnetperls.com/for
+        /// </summary>
+        /// <param name="arrayToSum"></param>
+        /// <returns></returns>
+        int SumArray2(int[] arrayToSum)
+        {
+            return 0;
+        }
     }
 }
